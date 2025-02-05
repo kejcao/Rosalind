4bddacb34a48b74b2e7a01f234601c9fe9ce0b07
Kevin Cao <kjcao@proton.me>
2025-02-05 17:05:33 -0500

    Add

---
+++ orf.hs
@@ -0,0 +1,90 @@
+-- https://rosalind.info/problems/orf/
+
+import Data.List (nub)
+
+input = "GCTACAGCGTATTTCGCTCCTTACGATGGTCGTCTTTTCTCGGGTACGTAGGCGGTATATCTGTCTGGACCTCTCGAGTCGTAACAGCTCAACAGCACTGCGGCGGACTCCAAAGTTATTTTAATTTCTTTAATTCAACATTTTACAAGGTTATACTTCATGGGTTTGAAGCACTTAACCTGACACACTCTATGGATCACAAATAAACTATTAGGTTGGATGCTAAAGATCTTGACGGGAACCGACTAGGTTTAATCATCATTAGGGAGTTTATTTTCATCTGGACCCCGTCAACAGTCCGCGTAGCTACGATAAGGTCGTAGTATGTTTGCGTACGAGCTTGCGCGGGCGGTCGCTGACAGCTATCGAATCTGGTCAGGCAGAGTCCGCATATTCGATGTGTTTTAATTAAGCAAACAATGTGAGAAGACGCAAATAGCCGGTAGCTACCGGCTATTTGCGTCTTCTCACATGGCGTACACGCAGCCGAGCGGGACAATGGGCGTACAGAAACCCACCTCGGAATGGCTTATAGCCGATACGTGACTTCTATTCGTATATGAAAGCTTATCCATATTATGGTTGTCTTCTTAACCGATCTGCTTTTTACAATCCCGGCACGGGTAGTCATTCGAAACGATAATTTAATGATCCTCACTGAGTCTACACGGTGAGTTGGTGCCGAAGCCTCCGTAAGTTGAGCTCCAACTTATACCACCTGTGAATCACCAGACTTCCTCATTTCATCCCGCCGGCCGGTCTCCCTGGCCCTTCTACCTTCTGCGCATGCAACGAGCCTAAGTAAGTGGTCCGGATGGCGTCTTTGTGCACCTCTCTCCCCTTATACTAAGGTCTGTCACGGGGGTAATTTCAACTCTATCTGGGATGAAGC"
+
+f "TTT" = "F"
+f "TTC" = "F"
+f "TTA" = "L"
+f "TTG" = "L"
+f "TCT" = "S"
+f "TCC" = "S"
+f "TCA" = "S"
+f "TCG" = "S"
+f "TAT" = "Y"
+f "TAC" = "Y"
+f "TAA" = "Stop"
+f "TAG" = "Stop"
+f "TGT" = "C"
+f "TGC" = "C"
+f "TGA" = "Stop"
+f "TGG" = "W"
+f "CTT" = "L"
+f "CTC" = "L"
+f "CTA" = "L"
+f "CTG" = "L"
+f "CCT" = "P"
+f "CCC" = "P"
+f "CCA" = "P"
+f "CCG" = "P"
+f "CAT" = "H"
+f "CAC" = "H"
+f "CAA" = "Q"
+f "CAG" = "Q"
+f "CGT" = "R"
+f "CGC" = "R"
+f "CGA" = "R"
+f "CGG" = "R"
+f "ATT" = "I"
+f "ATC" = "I"
+f "ATA" = "I"
+f "ATG" = "M"
+f "ACT" = "T"
+f "ACC" = "T"
+f "ACA" = "T"
+f "ACG" = "T"
+f "AAT" = "N"
+f "AAC" = "N"
+f "AAA" = "K"
+f "AAG" = "K"
+f "AGT" = "S"
+f "AGC" = "S"
+f "AGA" = "R"
+f "AGG" = "R"
+f "GTT" = "V"
+f "GTC" = "V"
+f "GTA" = "V"
+f "GTG" = "V"
+f "GCT" = "A"
+f "GCC" = "A"
+f "GCA" = "A"
+f "GCG" = "A"
+f "GAT" = "D"
+f "GAC" = "D"
+f "GAA" = "E"
+f "GAG" = "E"
+f "GGT" = "G"
+f "GGC" = "G"
+f "GGA" = "G"
+f "GGG" = "G"
+f _ = "Stop"
+
+complement = reverse . map f
+  where
+    f 'A' = 'T'
+    f 'T' = 'A'
+    f 'C' = 'G'
+    f 'G' = 'C'
+
+seqs [] = []
+seqs l
+  | take 3 l == "ATG" = l : seqs (tail l)
+  | otherwise = seqs (tail l)
+
+group [] = []
+group l = take 3 l : group (drop 3 l)
+
+main = mapM_ putStrLn $ nub $ map (concat . takeWhile (/= "Stop")) $ filter ("Stop" `elem`) chunks
+  where
+    chunks = map (map f . group) $ seqs input ++ seqs (complement input)
